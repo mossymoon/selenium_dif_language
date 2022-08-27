470dac308f6335eba6dfb9ef4a151a3bf486e301
mossymoon <stephan.sergienko@gmail.com>
2022-08-27 19:59:30 +0300

    final_commit

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,1 +1,12 @@
-# запуск тестов по тестированию языков
+import time
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_language_interface(browser):
+    browser.get(link)
+    time.sleep(30)
+    element = browser.find_element(By.XPATH, "//*[@id='add_to_basket_form']/button")
+    assert element, "Button Add to Basket is not presented"
